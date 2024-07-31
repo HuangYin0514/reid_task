63bbb86ac40d12729d86d4af190ed4e31f17d7ac
HuangYin <1071324110@qq.com>
2024-07-31 09:58:01 +0800

    feat: remove torchvision import in model.py

--- ex_001_4/model.py
+++ ex_001_4/model.py
@@ -1,7 +1,6 @@
 import torch
 import torch.nn.functional as F
 from torch import nn
-from torchvision import models
 
 import network
 
--- ex_001_5/model.py
+++ ex_001_5/model.py
@@ -4,7 +4,6 @@ import numpy as np
 import torch
 import torch.nn.functional as F
 from torch import Tensor, nn
-from torchvision import models
 
 import network
 
