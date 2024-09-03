1f166f02626840f35f579d152bba1e9f46f4e12f
HuangYin0514 <1071324110@qq.com>
2024-09-03 15:17:50 +0800

    (修复了model.py中的计算错误，优化了特征整合方式)

--- ex_002_20/model.py
+++ ex_002_20/model.py
@@ -101,7 +101,8 @@ class Integrate_feats_module(nn.Module):
         # Weights
         # weights = torch.ones(15, 4, device=self.config.device)  # (chunk_size, 4)
         # print("backbone_cls_score", backbone_cls_score[torch.arange(bs), pids].shape)
-        weights = backbone_cls_score[torch.arange(bs), pids].view(chunk_size, 4) * 5  # (chunk_size, 4)
+        # weights = backbone_cls_score[torch.arange(bs), pids].view(chunk_size, 4) * 5  # (chunk_size, 4)
+        weights = backbone_cls_score[torch.arange(bs), pids].view(chunk_size, 4)  # (chunk_size, 4)
         weights = torch.exp(weights)  # (chunk_size, 4)
         # print("weights", weights)
         weights_norm = torch.softmax(weights, dim=1)
