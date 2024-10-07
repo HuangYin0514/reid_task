d2314d36ee9a691e8a348163bae56b8544d30c56
HuangYin0514 <1071324110@qq.com>
2024-10-07 17:17:33 +0800

    (修复了model.py中的计算错误，优化了特征整合方式)

--- runs.sh
+++ runs.sh
@@ -8,6 +8,7 @@
 
 python ex_003_28/train.py --config ex_003_28/config.py
 python ex_003_29/train.py --config ex_003_29/config.py
-# python ex_003_30/train.py --config ex_003_30/config.py
+python ex_003_30/train.py --config ex_003_30/config.py
+python ex_003_31/train.py --config ex_003_31/config.py
 
 sh other_runs.sh
